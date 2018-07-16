c678d0ca5085b6d0934bc4516ccd0b23d23a3d65
17947 <jing.wang10@hand-china.com>
2018-07-16 16:37:03 +0800

    wj

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 docker run -dti --name wj05209 -p 127.0.0.1:1313:1313 --expose 1313  hugo
-#sh -c "cd /tmp/jingwang0521.github.io && hugo server --theme=tinyce-hugo-theme  --buildDrafts --watch "
+#sh -c "cd /tmp/jingwang0521.github.io && hugo server --theme=polymer  --buildDrafts --watch "
 hugo server --theme=tinyce-hugo-theme  --buildDrafts --watch
 #docker attach wj05209
 #cd /tmp/jingwang0521.github.io 
